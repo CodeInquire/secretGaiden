be602118e7c1e0e33c587debb3b95e89145b5f5d
CodeInquire <108208264+CodeInquire@users.noreply.github.com>
2023-03-21 18:02:50 -0700

    Update MAIN.py

--- MAIN.py
+++ MAIN.py
@@ -1,3 +1,10 @@
-import pygame
+import pygame, json,
+from sys import exit
 from pygame.locals import *
 from random import randint, choice
+
+def MAIN():
+	# game code here
+	pass
+
+MAIN() if __name__ == '__main__' else print('Not Main File')
